052545880a11b529a7d970551e0b49d02460d12a
Tamires <tamrs_cunha32@msn.com>
2020-07-25 12:35:11 -0300

    alte

--- Codenation/Controllers/WeatherForecastController.cs
+++ Codenation/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Codenation.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freeeeezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
